6e6bef015120066c54bf092cbd5527038ea42f39
justp <blits9000@gmail.com>
2019-11-05 10:42:11 +0200

    everything working

--- app/src/main/java/com/example/mycheflogin/ShowRecipesActivity.java
+++ app/src/main/java/com/example/mycheflogin/ShowRecipesActivity.java
@@ -1,14 +1,97 @@
 package com.example.mycheflogin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ShowRecipesActivity extends AppCompatActivity {
+    private BottomNavigationView menuhome, menutophome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_recipes);
+
+        menuhome = (BottomNavigationView) findViewById(R.id.menu_Pers);
+        menutophome = (BottomNavigationView) findViewById(R.id.topNav);
+
+        menutophome.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.helpItem:
+                        menuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                            @Override
+                            public boolean onMenuItemClick(MenuItem menuItem) {
+                                Intent intent = new Intent(ShowRecipesActivity.this, HelpActivity.class);
+                                startActivity(intent);
+                                return true;
+                            }
+                        });
+                        break;
+                    case R.id.searchtop:
+                        menuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                            @Override
+                            public boolean onMenuItemClick(MenuItem menuItem) {
+                                Intent intent = new Intent(ShowRecipesActivity.this, SearchActivity.class);
+                                startActivity(intent);
+                                return true;
+                            }
+                        });
+                        break;
+                }
+
+                return true;
+            }
+        });
+
+        menuhome.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()){
+                            case R.id.homeItem:
+                                item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                                    @Override
+                                    public boolean onMenuItemClick(MenuItem menuItem) {
+                                        Intent intent = new Intent(ShowRecipesActivity.this, HomeActivity.class);
+
+                                        return true;
+                                    }
+                                });
+                                break;
+
+                            case R.id.accountItem:
+                                item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                                    @Override
+                                    public boolean onMenuItemClick(MenuItem menuItem) {
+                                        Intent intent = new Intent(ShowRecipesActivity.this, PersonalActivity.class);
+                                        startActivity(intent);
+                                        return true;
+                                    }
+                                });
+                                break;
+
+                            case R.id.recipeItem:
+                                item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                                    @Override
+                                    public boolean onMenuItemClick(MenuItem menuItem) {
+                                        Intent intent = new Intent(ShowRecipesActivity.this, FavouritesActivity.class);
+                                        startActivity(intent);
+                                        return true;
+                                    }
+                                });
+                                break;
+
+                        }
+                        return true;
+                    }
+                }
+        );
     }
 }
